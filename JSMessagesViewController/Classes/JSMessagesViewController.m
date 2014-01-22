a03bed879332978c86304b54d02fe8a1d814e9c4
scot bond <apalgm@gmail.com>
2014-01-23 01:21:08 +0200

    tableView frame size must be the same as the container frame view. No reason to reduce it's height in account for the inputView while we can change tableView bottom content insets as being done anyway.

--- JSMessagesViewController/Classes/JSMessagesViewController.m
+++ JSMessagesViewController/Classes/JSMessagesViewController.m
@@ -60,13 +60,15 @@
     _loadingView = YES; // will be set to NO upon viewDidAppear
     
     CGSize size = self.view.frame.size;
-    
+
     JSMessageInputViewStyle inputViewStyle = [self.delegate inputViewStyle];
     CGFloat inputViewHeight = (inputViewStyle == JSMessageInputViewStyleFlat) ? 45.0f : 40.0f;
     
-    CGRect tableFrame = CGRectMake(0.0f, 0.0f, size.width, size.height - inputViewHeight);
+    CGRect tableFrame = CGRectMake(0.0f, 0.0f, size.width, size.height);
 	JSMessageTableView *tableView = [[JSMessageTableView alloc] initWithFrame:tableFrame style:UITableViewStylePlain];
 	tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    tableView.contentInset = [self tableViewInsetsWithBottomValue:inputViewHeight];
+    tableView.scrollIndicatorInsets = [self tableViewInsetsWithBottomValue:inputViewHeight];
 	tableView.dataSource = self;
 	tableView.delegate = self;
 	[self.view addSubview:tableView];
@@ -545,8 +547,7 @@
 																  inputViewFrame.size.height);
 
                          [self setTableViewInsetsWithBottomValue:self.view.frame.size.height
-                                                                - self.messageInputView.frame.origin.y
-                                                                - inputViewFrame.size.height];
+                                                                - self.messageInputView.frame.origin.y];
                      }
                      completion:nil];
 }
